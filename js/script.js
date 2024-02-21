b338f815116f27c3bf3c6aa0804d1a7d3dd5870d
federico_viganò <fede_viga@hotmail.it>
2024-02-21 14:23:30 +0100

    stampare in pagina singola mail

--- js/script.js
+++ js/script.js
@@ -14,8 +14,21 @@ createApp({
 
         return {
             
-            
+            email: "",
+
         }
+    },
+
+    mounted() {
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+
+            console.log(result)
+
+            this.email = result.data.response;
+
+        })
+
     }
-    
+
 }).mount('#app');
